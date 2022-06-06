000f1bfa03e16a7c9386911b008f03d158d2eafa
MrKerrVale <41432878+MrKerrVale@users.noreply.github.com>
2022-06-06 12:07:30 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showString("Mr Kerr")
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showString("Mr Burrin smells")
+    }
 })
